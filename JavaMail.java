3187998b65530f13b31a6c254261c7254c929973
Romulus-Andrei Bacila <dey.bacila09@yahoo.com>
2021-02-26 20:06:02 +0200

    first commit

---
+++ JavaMail.java
@@ -0,0 +1,13 @@
+package javatutorial.javamail;
+
+public class JavaMail {
+    public static void main(String[] args) throws Exception{
+        //JavaMailUtil.sendMail("teofanaekart@gmail.com");
+        //JavaMailUtil.sendMail("dey.bacila09@yahoo.com");
+        //JavaMailUtil.sendMail("teodorauscat@yahoo.com");
+        //JavaMailUtil.sendMail("george.bacila@yahoo.com");
+        //JavaMailUtil.sendMail("criss.bacila@yahoo.com");
+        //JavaMailUtil.sendMail("aturcsa@gmail.com");
+
+    }
+}
---
+++ JavaMailUtil.java
@@ -0,0 +1,53 @@
+package javatutorial.javamail;
+
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.net.ssl.ExtendedSSLSession;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//import static javatutorial.javamail.JavaMailUtil.prepareMessage;
+
+public class JavaMailUtil {
+    public static void sendMail(String recepient) throws Exception{
+        System.out.println("Sending message...");
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccEmail = "andreibacilatest@gmail.com";
+        String myPassword = "Laptedecapra123";
+        ///ExtendedSSLSession session = ExtendedSSLSession.getInstance() ;
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return  new PasswordAuthentication(myAccEmail,myPassword);
+            }
+        });
+
+        Message message = prepareMessage(session, myAccEmail, recepient);
+
+        Transport.send(message);
+        System.out.println("Message sent succesfully");
+    }
+    private static Message prepareMessage(Session session, String myAccEmail, String recepient){
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("My first Email from java app");
+            message.setText("Salutari, \n tocmai ce ai primit primul meu mail trimis dintr-o aplicatie java");
+            return message;
+        } catch (Exception ex){
+            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        return null;
+    }
+}
